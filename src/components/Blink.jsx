0cc894d26bce576cd1c097180e370a7a4ec50e70
Rafael Morales <rpara@freewiretech.com>
2021-03-23 20:29:42 -0600

    feature: Is better to use CSS for this task, however I used (bad practice), for the second legend useEffect but it was just a simple example that I would never use

--- src/components/Blink.jsx
+++ src/components/Blink.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const Blink = (props) => {
+
+    const legend = props.children;
+    const [showLegend, setShowLegend] = useState(true);
+
+    useEffect(() => {
+        const blinking = () => {
+            setTimeout( () => {
+                setShowLegend( !showLegend )
+                blinking(); 
+            }, 5000);
+        };
+
+        blinking(); 
+    })
+
     return(
-    <div className="react-blink">{props.children}</div>
+        <div>
+             { showLegend ? ( <div className="react-blink"> {legend} </div> ) : null }
+        </div>
     )
 }
